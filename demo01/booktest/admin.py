84de7585218e58820c39719152e0a3d599bc588d
cjb <1451055806@qq.com>
2019-04-16 15:49:55 +0800

    使用shell命令向数据库添加内容 在admin.py中注册类名 进入Django后台管理界面

--- demo01/booktest/admin.py
+++ demo01/booktest/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Hero)
+
 
 """
 少量代码管理强大后台
+
+需要将特定数据模型注册才能后台管理
+
 """
 
--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -5,7 +5,10 @@ from django.db import models
 
 class Book(models.Model):
     book_title = models.CharField(max_length=20)
-    bool_pubdate = models.DateTimeField(auto_now_add=True)
+    book_pubdate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.book_title
 
 
 class Hero(models.Model):
@@ -14,6 +17,9 @@ class Hero(models.Model):
     content = models.CharField(max_length=50)
     # 外键 第一个参数：关联的表名 第二个参数：删除类型
     book = models.ForeignKey("Book", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 """
@@ -27,6 +33,21 @@ ORM 对象中的O
 4、生成迁移文件 python manage.py makemigrations
 5、执行迁移 python manage.py migrate
 
-
+"""
 
 """
+shell命令测试：
+进入shell命令： python manage.py shell  不需要运行项目就可以操作数据库
+导入类： from booktest.models import *
+查找所有行 表名.objects.all()
+根据主键查找 表名.objects.get(pk=主键名)
+添加对象到数据库 对象.save()
+修改对象到数据库 对象.列名=修改的值  对象.save()
+删除 对象.delete()
+
+若存在一对多关系： 一方.多方类名全小写_set.all() 可查询相应信息
+
+类名.objects.create(列名=值)  不需要save()
+"""
+
+
