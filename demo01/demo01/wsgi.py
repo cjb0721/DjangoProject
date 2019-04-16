d8648f2c60df1ac5d32edd29b70ba2c0c2a4ae91
cjb <1451055806@qq.com>
2019-04-16 09:49:14 +0800

    创建demo01项目

---
+++ demo01/demo01/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for demo01 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo01.settings')
+
+application = get_wsgi_application()
+
+"""
+Web 服务器会指定特定的WSGI
+WSGI指定Web应用入口函数
+"""
