8e317acf036106f9b02ce23b1f83ddb65f68094f
cjb <1451055806@qq.com>
2019-04-17 15:44:54 +0800

    创建模板，使用模板测试index.html首页

--- demo01/booktest/views.py
+++ demo01/booktest/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
+from django.template import loader
+
 # Create your views here.
 
 
 def index(request):
     print("请求", request)
-    return HttpResponse("首页")
+    # return HttpResponse("首页")
+    # 1、加载模板
+    indexpage = loader.get_template('booktest/index.html')
+    body = {"username": "ggb"}
+    # 2、使用变量渲染模板
+    result = indexpage.render(body)
+    # 3、返回模板
+    return HttpResponse(result)
 
 
 def list(request):
@@ -24,5 +33,19 @@ def detail(request, id):
 
 """
 视图函数
+将函数和路由绑定
+"""
 
 """
+使用模板：
+1、创建模板文件夹templates
+2、配置模板目录 在settings.py中添加 os.path.join(BASE_DIR, 'templates')
+3、创建项目模板目录，创建模板
+
+    加载模板 temp = loader.get_template()
+    使用变量渲染模板 result = temp.render({})
+    返回模板  return HttpResponse(result)
+
+"""
+
+
