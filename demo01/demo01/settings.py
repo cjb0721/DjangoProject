533945255ff717adca0d88e8a73cd6a66b83df87
cjb <1451055806@qq.com>
2019-04-17 14:58:56 +0800

    配置MySQL数据库

--- demo01/demo01/settings.py
+++ demo01/demo01/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'demo01.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'demo01db',
-    #     'USER': 'root',
-    #     'PASSWORD': '123456',
-    #     'HOST': '',
-    #     'PORT': '',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'demo01db',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        # 'HOST': '',
+        # 'PORT': '',
+    }
 }
 
 
