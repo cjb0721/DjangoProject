33ce13ba7d3b14b18b5c5f7066e9cef4946dbcb6
cjb <1451055806@qq.com>
2019-04-20 12:00:45 +0800

    问题与选项的添加，删除，编辑功能

--- demo02/polls/urls.py
+++ demo02/polls/urls.py
@@ -8,4 +8,16 @@ urlpatterns = [
     url(r'vote/(\d+)/$', views.vote, name="vote"),
     url(r'detail/$', views.detail, name="detail"),
     url(r'result/(\d+)/$', views.result, name="result"),
+
+    url(r'addquestion/$', views.addquestion, name='addquestion'),
+    url(r'addquestionhander/$', views.addquestionhander, name='addquestionhander'),
+    url(r'deletequestion/(\d+)/$', views.deletequestion, name='deletequestion'),
+    url(r'modifyquestion/(\d+)/$', views.modifyquestion, name='modifyquestion'),
+    url(r'modifyquestionhander/(\d+)/$', views.modifyquestionhander, name='modifyquestionhander'),
+
+    url(r'votemanage/(\d+)/$', views.votemanage, name='votemanage'),
+    url(r'addchoice/(\d+)/$', views.addchoice, name='addchoice'),
+    url(r'addchoicehander/$', views.addchoicehander, name='addchoicehander'),
+    url(r'deletechoice/(\d+)/$', views.deletechoice, name='deletechoice'),
+
 ]
--- demo02/polls/views.py
+++ demo02/polls/views.py
@@ -17,17 +17,21 @@ def vote(request, id):
 
 
 def detail(request):
-    vid = request.POST['vote']
-    qid = request.POST['qid']
-    # print(vid, qid)
-
-    s = Select.objects.get(pk=vid)
-    print(s.vote, type(s.vote))
-    s.vote += 1
-    s.save()
-    q = Question.objects.get(pk=qid)
-
-    return HttpResponseRedirect('/polls/result/'+str(q.id)+'/', {})
+    if 'vote' in request.POST:
+        vid = request.POST['vote']
+        qid = request.POST['qid']
+        # print(vid, type(vid))
+        if isinstance(vid, str):
+            s = Select.objects.get(pk=vid)
+            # print(s.vote, type(s.vote))
+            s.vote += 1
+            s.save()
+            q = Question.objects.get(pk=qid)
+            return HttpResponseRedirect('/polls/result/'+str(q.id)+'/', {})
+        else:
+            return HttpResponse("还没有投票")
+    else:
+        return HttpResponse("还没有选项信息")
 
 
 def result(request, id):
@@ -35,7 +39,69 @@ def result(request, id):
     return render(request, "polls/result.html", {'qs':q})
 
 
+def addquestion(request):
+    return render(request, 'polls/addquestion.html', {})
 
 
+def addquestionhander(request):
+    title = request.POST['title']
+    print(title)
+    q = Question()
+    q.text = title
+    q.save()
+    return HttpResponseRedirect('/polls/', {'ql', Question.objects.all()})
+
+
+def deletequestion(request, id):
+    Question.objects.get(pk=id).delete()
+    l = Question.objects.all()
+    return HttpResponseRedirect('/polls/', {'ql': l})
+
+
+def modifyquestion(request, id):
+    q = Question.objects.get(pk=id)
+    return render(request, 'polls/modifyquestion.html', {'qt': q})
+
+
+def modifyquestionhander(request, id):
+    qt = Question.objects.get(pk=id)
+    qt.text = request.POST['title']
+    qt.save()
+    l = Question.objects.all()
+    return HttpResponseRedirect('/polls/', {'ql': l})
+
+
+def votemanage(request, id):
+    q = Question.objects.get(pk=id)
+    s = q.select_set.all()
+    return render(request, 'polls/votemanage.html', {'question': q, 'sl': s})
+
+
+def addchoice(request, id):
+    q = Question.objects.get(pk=id)
+    return render(request, 'polls/addchoice.html', {'qs': q})
+
+
+def addchoicehander(request):
+    q = Question.objects.get(pk=request.POST['qid'])
+    sl = q.select_set.all()
+    s = Select()
+    s.item = request.POST['item']
+    s.vote = request.POST['vote']
+    s.question = q
+    s.save()
+    return HttpResponseRedirect('polls/votemanage/'+str(q.id)+'/', {'question': q, 'sl': sl})
+
+
+def deletechoice(request, id):
+    l = Select.objects.get(pk=id)
+    q = l.question
+    l.delete()
+    sl = q.select_set.all()
+    return HttpResponseRedirect('polls/votemanage/'+str(q.id)+'/', {'question': q, 'sl': sl})
+
+
+def modifychoice(request):
+    pass
 
 
